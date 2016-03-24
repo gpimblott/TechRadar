56244b119227ad69d157fe50348edfbdd6aa75cc
pimblottg <g.pimblott@kainos.com>
2016-03-24 13:52:14 +0000

    Change port and address

--- server.js
+++ server.js
@@ -29,16 +29,16 @@ var TechRadar = function () {
      */
     self.setupVariables = function () {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+       // self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
         self.port = process.env.OPENSHIFT_NODEJS_PORT || 8090;
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        }
-        ;
+        //if (typeof self.ipaddress === "undefined") {
+        //    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+        //    //  allows us to run/test the app locally.
+        //    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        //    self.ipaddress = "127.0.0.1";
+        //}
+        //;
     };
 
 
@@ -120,7 +120,7 @@ var TechRadar = function () {
      */
     self.start = function () {
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function () {
+        self.app.listen(self.port,  function () {
             console.log('%s: Node server started on %s:%d ...',
                 Date(Date.now()), self.ipaddress, self.port);
         });
