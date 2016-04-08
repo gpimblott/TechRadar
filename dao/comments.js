490e9a922629c427807de19b53d1c42ab3872669
pimblottg <g.pimblott@kainos.com>
2016-04-08 21:24:52 +0100

    Correct login and add comment pages

---
+++ dao/comments.js
@@ -0,0 +1,25 @@
+var db = require('../config/dbConfig.js');
+var pg = require('pg');
+var dbhelper = require('../dao/dbhelper.js');
+
+/**
+ * Database routines for 'Category's'
+ * @constructor
+ */
+var Category = function () {
+};
+
+
+Category.getValuesForCategory = function (done) {
+    var sql = "SELECT * FROM categories ";
+
+    dbhelper.query(sql, [],
+        function (results) {
+            done(results);
+        },
+        function (error) {
+            console.log(error);
+        });
+}
+
+module.exports = Category;
