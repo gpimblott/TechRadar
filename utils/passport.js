6b84ad61a0298f2ed4c42706695fc7bb025c905f
pimblottg <g.pimblott@kainos.com>
2016-04-23 21:38:46 +0100

    change env variable loading

---
+++ utils/passport.js
@@ -0,0 +1,33 @@
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var users = require('../dao/users.js');
+
+
+passport.use(new Strategy(
+    function (username, password, cb) {
+        users.findByUsername(username, function (err, user) {
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+    }));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    users.findById(id, function (err, user) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, user);
+    });
+});
