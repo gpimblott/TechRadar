1043a5b8964bc51b6a58ff07cc174d39d0b6c09d
pimblottg <g.pimblott@kainos.com>
2016-05-22 11:56:59 +0100

    Fixed route ordering

--- routes/web/comment-routes.js
+++ routes/web/comment-routes.js
@@ -11,12 +11,23 @@ var CommentRoutes = function () {
 CommentRoutes.createRoutes = function (self) {
 
     /**
+     * Add a new comment for technology page
+     */
+    self.app.get('/comments/add/:id', security.isAuthenticated,
+        function (req, res) {
+            var num = req.params.id;
+            technology.getById(num, function (value) {
+                res.render('pages/addComment', {technology: value, user: req.user});
+            });
+        });
+
+    /**
      * Comment page partial
      */
     self.app.get('/comments/:technologyId/:page', security.isAuthenticated,
         function (req, res) {
-            var techid = sanitizer( req.params.technologyId );
-            var pageNumber = sanitizer( req.params.page );
+            var techid = sanitizer(req.params.technologyId);
+            var pageNumber = sanitizer(req.params.page);
             comments.getForTechnology(techid, pageNumber, PAGE_SIZE, function (result) {
                 comments.getCountForTechnology(techid, function (countData) {
                     res.render('partials/comments', {
@@ -32,16 +43,7 @@ CommentRoutes.createRoutes = function (self) {
 
         });
 
-    /**
-     * Add a new comment for technology page
-     */
-    self.app.get('/comments/add/:id', security.isAuthenticated,
-        function (req, res) {
-            var num = req.params.id;
-            technology.getById(num, function (value) {
-                res.render('pages/addComment', {technology: value, user: req.user});
-            });
-        });
+
 };
 
 
