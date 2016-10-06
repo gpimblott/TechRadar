321dd8309aa0f6ad6aa2834b6b0d0a3531508f6d
Adrian Zabrocki <a.zabrocki@kainos.com>
2016-10-06 17:15:07 +0200

    Add versions-related API, scripts and routes

---
+++ handlers/api/softwareVersionsApiHandler.js
@@ -0,0 +1,34 @@
+var sanitizer = require('sanitize-html');
+var apiutils = require('./apiUtils.js');
+var versionsDao = require('../../dao/softwareVersions.js');
+
+var SoftwareVersionsApiHandler = function () {
+};
+
+/**
+ * Get all SoftwareVersions
+ */
+SoftwareVersionsApiHandler.getAllVersionsForTechnology = function (req, res) {
+    var techId = sanitizer(req.params.technology);
+    versionsDao.getAllForTechnology(techId, function (result) {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(result));
+    });
+};
+
+SoftwareVersionsApiHandler.addVersion = function (req, res) {
+    var techId = sanitizer(req.body.technology);
+    var name = sanitizer(req.body.name);
+    versionsDao.add(
+        techId, name, 
+        function (result, error) {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            if (error != null) {
+                res.end(JSON.stringify({success: false, error: error}));
+            } else {
+                res.end(JSON.stringify({success: true}));
+            }
+        });
+};
+
+module.exports = SoftwareVersionsApiHandler;
